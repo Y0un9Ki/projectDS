c854c4783fea3896c7f28dcb2f9bac1ed7a35eea
Y0un9Ki <srilankakim66@gmail.com>
2024-01-08 17:33:35 +0900

    use library for  visualization and each library pros and cons

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np 
+
+def main():
+    print(np.__version__)
+    print("Hello 가상환경")
+
+if __name__ == "__main__":
+    main()
+
+# 가상환경내에 numpy라는 라이브러리를 잘 다운받았나 확인하기 위해 사용했다.
+    
+
+
+
+data = {
+    data_color : ['a','b','c'],
+    data_set : [1,2,3],
+    data_label : ['']
+    
+}
+     
+
+print(data[1].values())
